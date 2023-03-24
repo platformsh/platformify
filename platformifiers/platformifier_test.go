fc046ee0989c5925cfb927812605b6b90377cea6
Andrea Soper <github@zendoodles.com>
2023-03-24 17:39:05 -0400

    Generic platformifier tests

--- platformifiers/platformifier_test.go
+++ platformifiers/platformifier_test.go
@@ -23,7 +23,7 @@ func TestNewPlatformifier(t *testing.T) {
 		{
 			"when the stack is empty",
 			fields{&models.Answers{Stack: ""}},
-			args{}, true,
+			args{}, false,
 		},
 		{
 			"when the stack is wrong",
@@ -31,27 +31,27 @@ func TestNewPlatformifier(t *testing.T) {
 			args{}, true,
 		},
 		{
-			"when a laravel platformifier is created successfully",
-			fields{&models.Answers{Stack: "laravel"}},
+			"when a generic platformifier is created successfully",
+			fields{&models.Answers{Stack: "generic"}},
 			args{}, false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			pfier, err := NewLaravelPlatformifier(tt.fields.answers)
+			pfier, err := NewPlatformifier(tt.fields.answers)
 			if err != nil && !tt.wantErr {
-				t.Errorf("NewLaravelPlatformifier error = #{err}, wantErr #{tt.wantErr}")
+				t.Errorf("NewPlatformifier error = #{err}, wantErr #{tt.wantErr}")
 			}
 			// Don't return a Platformifier if there's an error.
 			if tt.wantErr {
 				assert.Nil(t, pfier)
 			} else {
-				// Otherwise, make sure it's a LaravelPlatformifier.
-				assert.IsType(t, new(LaravelPlatformifier), pfier, "created object is not a LaravelPlatformifier")
+				// Otherwise, make sure it's a Platformifier.
+				assert.IsType(t, new(Platformifier), pfier, "created object is not a Platformifier")
 				// And ensure it implements the PlatformifierInterface.
 				var inter interface{} = pfier
 				_, pass := inter.(PlatformifierInterface)
-				assert.True(t, pass, "created LaravelPlatformifier but it does not implement PlatformifierInterface")
+				assert.True(t, pass, "created Platformifier but it does not implement PlatformifierInterface")
 			}
 		})
 	}
