c0066f775a0ce97bd1d26ccc14cf4ab773564fd4
Andrea Soper <github@zendoodles.com>
2023-05-08 09:33:25 -0500

    Create the validate command

---
+++ commands/validate.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"github.com/platformsh/platformify/internal/colors"
+	"github.com/platformsh/platformify/internal/validator"
+	"github.com/spf13/cobra"
+)
+
+var ValidateCmd = &cobra.Command{
+	Use:   "validate",
+	Short: "Validate your Platform.sh application config.",
+	Long: "Check the application yaml configuration files are valid for deploying an application to Platform.sh.\n\n" +
+		"This will check your git repository, and validate .platform.app.yaml, services.yaml and routes.yaml files.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var validator = validator.ConfigValidator{}
+		var ctx context.Context
+		ctx = colors.ToContext(
+			ctx,
+			cmd.OutOrStderr(),
+			cmd.ErrOrStderr(),
+		)
+		err := validator.Validate(ctx)
+		if err != nil {
+			fmt.Fprintln(cmd.ErrOrStderr(), colors.Colorize(colors.ErrorCode, err.Error()))
+			return fmt.Errorf("validation failed: #{err}")
+		}
+
+		return nil
+	},
+}
---
+++ internal/validator/validator.go
@@ -0,0 +1,10 @@
+package validator
+
+import "context"
+
+type ConfigValidator struct {
+}
+
+func (validator *ConfigValidator) Validate(ctx context.Context) error {
+	return nil
+}
