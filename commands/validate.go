0f67ca596c3facc34b9d51e11091f18502e2bac8
Andrea Soper <github@zendoodles.com>
2023-05-23 12:17:49 -0400

    Use the new validator package

--- commands/validate.go
+++ commands/validate.go
@@ -1,11 +1,13 @@
 package commands
 
 import (
-	"context"
 	"fmt"
-	"github.com/platformsh/platformify/internal/colors"
-	"github.com/platformsh/platformify/internal/validator"
+	"path"
+
 	"github.com/spf13/cobra"
+
+	"github.com/platformsh/platformify/internal/colors"
+	"github.com/platformsh/platformify/validator"
 )
 
 var ValidateCmd = &cobra.Command{
@@ -14,14 +16,7 @@ var ValidateCmd = &cobra.Command{
 	Long: "Check the application yaml configuration files are valid for deploying an application to Platform.sh.\n\n" +
 		"This will check your git repository, and validate .platform.app.yaml, services.yaml and routes.yaml files.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		var validator = validator.ConfigValidator{}
-		var ctx = context.Background()
-		ctx = colors.ToContext(
-			ctx,
-			cmd.OutOrStderr(),
-			cmd.ErrOrStderr(),
-		)
-		err := validator.Validate(ctx)
+		_, err := validator.ValidateFile(path.Ext(".platform.app.yaml"))
 		if err != nil {
 			fmt.Fprintln(cmd.ErrOrStderr(), colors.Colorize(colors.ErrorCode, err.Error()))
 			return fmt.Errorf("validation failed: #{err}")
