e43c09fa4e3f8face9a490ba7338eb40afde1b92
Andrea Soper <github@zendoodles.com>
2023-05-23 13:33:19 -0400

    Test all the stacks.

--- cmd/platformify/main_test.go
+++ cmd/platformify/main_test.go
@@ -5,33 +5,41 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	"github.com/platformsh/platformify/internal/question/models"
 	"github.com/platformsh/platformify/platformifier"
 	"github.com/platformsh/platformify/validator"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestYAMLOutput(t *testing.T) {
-	// Create a temporary directory to use as the output directory.
-	tempDir, err := os.MkdirTemp("", "yaml_output_test")
-	if err != nil {
-		t.Fatalf("Failed to create temporary directory: %v", err)
-	}
-	defer os.RemoveAll(tempDir)
+	for _, stack := range models.Stacks {
+		// Create a temporary directory to use as the output directory.
+		tempDir, err := os.MkdirTemp("", stack.Title())
+		if err != nil {
+			t.Fatalf("Failed to create temporary directory: %v", err)
+		}
 
-	// Run the command.
-	ui := &platformifier.UserInput{
-		WorkingDirectory: tempDir,
-	}
-	ctx := context.Background()
-	err = runApp(ctx, ui)
-	if err != nil {
-		t.Fatalf("App failed with error: %v", err)
-	}
+		// Run the command.
+		ui := &platformifier.UserInput{
+			Stack:            platformifier.Stack(stack),
+			WorkingDirectory: tempDir,
+		}
+		ctx := context.Background()
+		err = runApp(ctx, ui)
+		if err != nil {
+			t.Fatalf("App failed with error: %v", err)
+		}
 
-	// Validate the config.
-	invalid := validator.ValidateConfig(tempDir)
-	assert.NoError(t, err, "error while validating config: %v", invalid)
+		// Validate the config.
+		invalid := validator.ValidateConfig(tempDir)
+		assert.NoError(t, err, "error while validating config: %v", invalid)
+
+		rmErr := os.RemoveAll(tempDir)
+		if rmErr != nil {
+			t.Fatalf("Could not remove temporary directory: %v", tempDir)
+		}
+	}
 }
 
 func runApp(ctx context.Context, ui *platformifier.UserInput) error {
