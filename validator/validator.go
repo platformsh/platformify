253ea3a7f90398c7fa06d4961c063572a4cc41de
Andrea Soper <github@zendoodles.com>
2023-05-24 18:25:40 -0400

    provide the json schema files

--- validator/validator.go
+++ validator/validator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/hashicorp/go-multierror"
 	"gopkg.in/yaml.v3"
@@ -29,27 +30,42 @@ func ValidateFile(path string) (interface{}, error) {
 }
 
 // ValidateData checks to see if the unmarshalled data is valid config.
-func ValidateData(data interface{}, _ string) error {
+func ValidateData(data interface{}, schemaFile string) error {
 	if data == nil {
 		return fmt.Errorf("data should not be nil")
 	}
-	return nil
+
 }
 
 // ValidateConfig uses ValidateFile and ValidateData to check config for a given directory is valid Platform.sh config.
 func ValidateConfig(path string) error {
 	var errs error
-	files := []string{".platform.app.yaml", ".platform/services.yaml", ".platform/relationships.yaml"}
-	for _, file := range files {
-		data, fileErr := ValidateFile(filepath.Join(path, file))
+	for yamlFile, schemaFile := range schemaMap() {
+		data, fileErr := ValidateFile(filepath.Join(path, yamlFile))
 		if fileErr != nil {
-			errs = multierror.Append(errs, fmt.Errorf("problem with %v: %v", file, fileErr))
+			errs = multierror.Append(errs, fmt.Errorf("problem with %v: %v", yamlFile, fileErr))
 		}
-		dataErr := ValidateData(data, file)
+		dataErr := ValidateData(data, schemaFile)
 		if dataErr != nil {
-			errs = multierror.Append(errs, fmt.Errorf("configuration directive in %v is invalid: %v", file, dataErr))
+			errs = multierror.Append(errs, fmt.Errorf("configuration directive in %v is invalid: %v", yamlFile, dataErr))
 		}
 	}
 
 	return errs
+}
+
+// The schemaMap is a hash map of yaml config file paths to their respective openAPI json schema files.
+func schemaMap() map[string]string {
+	schemaPath := getSchemaDir()
+	var schemaMap = map[string]string{
+		".platform.app.yaml":      filepath.Join(schemaPath, "platformsh.application.json"),
+		".platform/services.yaml": filepath.Join(schemaPath, "platformsh.services.json"),
+		".platform/routes.yaml":   filepath.Join(schemaPath, "platformsh.routes.json"),
+	}
+	return schemaMap
+}
+
+func getSchemaDir() string {
+	_, filename, _, _ := runtime.Caller(1)
+	return filepath.Join(filepath.Dir(filename), "schema")
 }
