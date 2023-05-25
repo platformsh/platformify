f975602fba6f722c6784fa1a2b3bc88bfe90df93
Andrea Soper <github@zendoodles.com>
2023-05-25 11:46:18 -0400

    Fully validate against the schema

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.15.0
 	github.com/stretchr/testify v1.8.1
+	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -20,8 +21,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.4.0 // indirect
 	github.com/imdario/mergo v0.3.13 // indirect
@@ -41,6 +40,8 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
--- validator/validator.go
+++ validator/validator.go
@@ -1,17 +1,17 @@
 package validator
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 
-	"github.com/hashicorp/go-multierror"
+	"github.com/xeipuuv/gojsonschema"
 	"gopkg.in/yaml.v3"
 )
 
 // ValidateFile checks the file exists and is valid yaml, then returns the unmarshalled data.
-func ValidateFile(path string) (interface{}, error) {
+func ValidateFile(path string) ([]byte, error) {
 	// Does the file exist?
 	yamlData, err := os.ReadFile(path)
 	if err != nil {
@@ -19,7 +19,7 @@ func ValidateFile(path string) (interface{}, error) {
 	}
 
 	// Does the yaml unmarshal properly?
-	var data interface{}
+	var data []byte
 	err = yaml.Unmarshal(yamlData, &data)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal YAML data: %v", err)
@@ -30,12 +30,11 @@ func ValidateFile(path string) (interface{}, error) {
 }
 
 // ValidateData checks to see if the unmarshalled data is valid config.
-func ValidateData(data interface{}, schemaFile string) error {
+func ValidateData(data []byte, schemaFile string) error {
 	if data == nil {
 		return fmt.Errorf("data should not be nil")
 	}
-	println(schemaFile)
-	return nil
+	return validateWithSchema(data, schemaFile)
 }
 
 // ValidateConfig uses ValidateFile and ValidateData to check config for a given directory is valid Platform.sh config.
@@ -44,11 +43,11 @@ func ValidateConfig(path string) error {
 	for yamlFile, schemaFile := range schemaMap() {
 		data, fileErr := ValidateFile(filepath.Join(path, yamlFile))
 		if fileErr != nil {
-			errs = multierror.Append(errs, fmt.Errorf("problem with %v: %v", yamlFile, fileErr))
+			errs = errors.Join(errs, fmt.Errorf("problem with %v: %v", yamlFile, fileErr))
 		}
 		dataErr := ValidateData(data, schemaFile)
 		if dataErr != nil {
-			errs = multierror.Append(errs, fmt.Errorf("configuration directive in %v is invalid: %v", yamlFile, dataErr))
+			errs = errors.Join(errs, fmt.Errorf("configuration directive in %v is invalid: %v", yamlFile, dataErr))
 		}
 	}
 
@@ -67,6 +66,30 @@ func schemaMap() map[string]string {
 }
 
 func getSchemaDir() string {
-	_, filename, _, _ := runtime.Caller(1)
-	return filepath.Join(filepath.Dir(filename), "schema")
+	dir, _ := os.Getwd()
+	return filepath.Join(filepath.Dir(dir), "schema")
+}
+
+func validateWithSchema(data []byte, schemaFile string) error {
+	var errs error
+	// Load and compile the JSON schema
+	schemaLoader := gojsonschema.NewReferenceLoader("file://" + schemaFile)
+	documentLoader := gojsonschema.NewBytesLoader(data)
+
+	// Perform the validation
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		errs = errors.Join(errs, fmt.Errorf("failed to validate file: %w", err))
+	}
+
+	if !result.Valid() {
+		// Collect and format the validation errors
+		for _, err := range result.Errors() {
+			errs = errors.Join(errs, errors.New(err.String()))
+		}
+
+		return fmt.Errorf("%s file is invalid:\n%s", schemaFile, errs)
+	}
+
+	return errs
 }
