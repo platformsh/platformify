99775ff6d066b45e2789e88db3a1960a06fa75fd
Antonis Kalipetis <antonis.kalipetis@platform.sh>
2023-08-23 16:42:34 +0300

    Add @thomasdiluccio suggestion
    Co-authored-by: Thomas di Luccio <thomas@zenhysteria.fr>

--- internal/question/services.go
+++ internal/question/services.go
@@ -37,7 +37,7 @@ func (q *Services) Ask(ctx context.Context) error {
 	fmt.Fprintln(out)
 
 	question := &survey.MultiSelect{
-		Message: "Select all the services you are using",
+		Message: "Select all the services you are using:",
 		Options: models.ServiceNames.AllTitles(),
 	}
 
